ae0a4ed0a5bf8424ddaf0ac21f9e220a6693039e
Ivan Iskandar <ivan@prjkt.io>
2024-04-06 22:44:53 +0700

    Init shell builder only once
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

--- theme/src/main/java/projekt/commons/theme/ThemeApplication.kt
+++ theme/src/main/java/projekt/commons/theme/ThemeApplication.kt
@@ -62,34 +62,26 @@ open class ThemeApplication : Application() {
             pieRootSupported: Boolean = false,
             rootSupported: Boolean = false
         ): Boolean {
+            if (backend != null) return true
+
             Shell.enableVerboseLogging = instance.isApplicationDebugable
-            val builder = Shell.Builder.create()
-                .setTimeout(SHELL_TIMEOUT)
             // TODO: Choose used backend if multiple supported
             backend = if (andromedaSamsungSupported && isSamsung && !isAtleastPie && isAndromeda &&
                     AndromedaClient.initialize(instance)) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 AndromedaSamsungBackend()
             } else if (andromedaSupported && !isAtleastPie && isAndromeda && AndromedaClient.initialize(instance)) {
-                builder
-                    .setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 AndromedaBackend()
             } else if (substratumServiceSupported && isSubstratumService) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 SubstratumServiceBackend()
             } else if (pieRootSupported && isRooted && isAtleastPie) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR)
                 PieRootBackend()
             } else if (rootSupported && isRooted && !isAtleastPie) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR)
                 RootBackend()
             } else if (synergySupported && isSynergyInstalled) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 SynergyBackend()
             } else {
                 return false
             }
-            Shell.setDefaultBuilder(builder)
             return true
         }
 
@@ -108,41 +100,43 @@ open class ThemeApplication : Application() {
             rootSupported: Boolean = false
         ): Boolean {
             Shell.enableVerboseLogging = instance.isApplicationDebugable
-            val builder = Shell.Builder.create().setTimeout(SHELL_TIMEOUT)
             // TODO: Choose used backend if multiple supported
             backend = if (andromedaSamsungSupported && isSamsung && !isAtleastPie && isAndromeda &&
                     AndromedaClient.initialize(instance)) {
                 if (instance.checkSelfPermission(ACCESS_PERMISSION) != PERMISSION_GRANTED) {
                     return false
                 }
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 AndromedaSamsungBackend()
             } else if (andromedaSupported && !isAtleastPie && isAndromeda &&
                     AndromedaClient.initialize(instance)) {
                 if (instance.checkSelfPermission(ACCESS_PERMISSION) != PERMISSION_GRANTED) {
                     return false
                 }
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 AndromedaBackend()
             } else if (substratumServiceSupported && isSubstratumService) {
                 // Should've check for permissive settings too but we already catch everything so
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 SubstratumServiceBackend()
             } else if (pieRootSupported && isRooted && isAtleastPie) {
                 // Root will ask permission if needed
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR)
                 PieRootBackend()
             } else if (rootSupported && isRooted  && !isAtleastPie) {
                 // Root will ask permission if needed
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR)
                 RootBackend()
             } else if (synergySupported && isSynergyInstalled) {
-                builder.setFlags(Shell.FLAG_REDIRECT_STDERR or Shell.FLAG_NON_ROOT_SHELL)
                 SynergyBackend()
             } else {
                 return false
             }
             return true
+        }
+
+        private fun initShell() {
+            if (Shell.getCachedShell() == null) {
+                val builder = Shell.Builder.create()
+                    .setFlags(Shell.FLAG_REDIRECT_STDERR)
+                    .setTimeout(SHELL_TIMEOUT)
+                Shell.setDefaultBuilder(builder)
+            }
         }
     }
 
@@ -152,5 +146,6 @@ open class ThemeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        initShell()
     }
 }
